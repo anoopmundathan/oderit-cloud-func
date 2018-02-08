56e440b8cb3db023d19381524d1faddaf948b645
Anoop Mundathan <anoop.mundathan@gmail.com>
2018-02-08 07:44:38 +0000

    Validate phone number and create user

--- create_user.js
+++ create_user.js
@@ -1,3 +1,18 @@
+const admin = require("firebase-admin");
+
 module.exports = function(req, res) {
-  res.send(req.body)
+
+  // Verify user provided phone
+  if(!req.body.phone) {
+    return res.status(422).send({ error: 'Bad Input' })
+  }
+
+  // Format the phone number 
+  const phone = String(req.body.phone).replace(/[^\d]/g,"");
+
+  // Create New User and return
+  admin.auth().createUser({ uid: phone })
+    .then(user => res.send(user))
+    .catch(err => res.status(422).send({ error: err }))
+
 }
