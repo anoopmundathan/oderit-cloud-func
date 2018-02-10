8f24e7176bb8c3def527c7acd33af889de4ebf94
Anoop Mundathan <anoop.mundathan@gmail.com>
2018-02-10 08:25:48 +0000

    Send text message once order is received

--- order-confirmation.js
+++ order-confirmation.js
@@ -1,5 +1,28 @@
+const twilio = require("./twilio");
+
 module.exports = function(req, res) {
-    res.send({
-        message: "Your order is on way"
+
+    if(!req.body.phone || !req.body.basket) {
+        return res.status(422).send({ error: 'Bad Input' })
+    }
+
+    // Format the phone number 
+    const phone = String(req.body.phone).replace(/[^\d]/g,"");
+
+    twilio.messages.create({
+        body: `New order is received`,
+        to: phone,
+        from: '+441392690457'
+    }, (err) => { 
+        if(err) { 
+            return res.status(422).send(err) 
+        }
+
+        res.send({ 
+            basket: req.body.basket,
+            sucess: true 
+        });
+
     });
+
 }
